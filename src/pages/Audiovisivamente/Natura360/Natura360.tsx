25dfd1cb84d14141a1877de0ee6bd7b871cae475
Marcolux <mccolucci1@gmail.com>
2026-05-15 13:37:34 -0500

    Added new 360 video

--- src/pages/Audiovisivamente/Natura360/Natura360.tsx
+++ src/pages/Audiovisivamente/Natura360/Natura360.tsx
@@ -4,8 +4,7 @@ import "../../page.scss"
 import "./natura360.scss"
 
 const withPublicUrl = (p: string) => `${process.env.PUBLIC_URL}${p.startsWith("/") ? "" : "/"}${p}`
-const VIDEO_SRC_6 = 'https://pub-3e8f1f8594254c93a49fb4e5bef03ab0.r2.dev/Costa_Ripagnola_TEST%201%20700Mb%20for%20S3.mp4'
-const VIDEO_SRC_7 = 'https://pub-3e8f1f8594254c93a49fb4e5bef03ab0.r2.dev/Costa%20Ripagnola_file_da_1GB.mp4'
+const VIDEO_SRC_7 = 'https://pub-3e8f1f8594254c93a49fb4e5bef03ab0.r2.dev/Costa%20Ripagnola%20-%20Video%201.mp4'
 const POSTER_SRC  = withPublicUrl('/immagini-pagine/audiovisivamente/natura360/poster.jpg')
 
 const Natura360 = () => {
@@ -31,13 +30,8 @@ const Natura360 = () => {
             </section>
 
             <section className="natura360__player">
-                <p className="natura360__label">Costa Ripagnola TEST 1 Versione Marco S3 API Load</p>
-                <Player360 src={VIDEO_SRC_6} POSTER_SRC={POSTER_SRC} videoId="video360_1" />
-            </section>
-
-            <section className="natura360__player">
-                <p className="natura360__label">Costa Ripagnola S3 API Load over 1GB</p>
-                <Player360 src={VIDEO_SRC_7} POSTER_SRC={POSTER_SRC} videoId="video360_2" />
+                <p className="natura360__label">Costa Ripagnola - Video 1</p>
+                <Player360 src={VIDEO_SRC_7} POSTER_SRC={POSTER_SRC} videoId="video360_1" />
             </section>
         </div>
     )
